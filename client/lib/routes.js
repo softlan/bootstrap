fe850343388b7bbfa2350af64d058bf6d5839e13
Alex <Alex@ALEXHOME>
2016-09-15 04:18:26 +0300

    Добавьте файлы проекта.

---
+++ client/lib/routes.js
@@ -0,0 +1,19 @@
+﻿FlowRouter.route('/', {
+    name: 'home',
+    action() {
+        BlazeLayout.render('masterLayout', {
+            main: 'home',
+            nav: "nav"
+        });
+    }
+});
+
+FlowRouter.route('/features', {
+    name: 'features',
+    action() {
+        BlazeLayout.render('masterLayout', {
+            main: 'features',
+            nav: "nav"
+        });
+    }
+});
