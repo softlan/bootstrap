1e397933714b33d3300edf0107932c115f03e89f
Alex (home main) <Alex@ALEXHOME>
2016-09-15 05:07:09 +0300

    1

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,4 +1,37 @@
-﻿FlowRouter.route('/', {
+﻿var arr = [
+    {
+        path: '/',
+        name: 'home',
+            main: 'home',
+            nav: "nav"
+    },
+    {
+        path: '/features',
+        name: 'features',
+            main: 'features',
+            nav: "nav"
+    },
+];
+
+for (var i = 0; i < arr.length; i++) {
+
+    var path = i == 0 ? '/' : '/features';
+    var name = i == 0 ? 'home' : 'features';
+    var main = i == 0 ? 'home' : 'features';
+    var nav = i == 0 ? 'nav' : 'nav';
+
+    FlowRouter.route(path, {
+        name: name,
+        action() {
+            BlazeLayout.render('masterLayout', {
+                main: main,
+                nav: nav
+            });
+        }
+    });
+}
+
+/*FlowRouter.route('/', {
     name: 'home',
     action() {
         BlazeLayout.render('masterLayout', {
@@ -16,4 +49,4 @@ FlowRouter.route('/features', {
             nav: "nav"
         });
     }
-});
+});*/
